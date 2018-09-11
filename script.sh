5d41fa0e430a77626883de6fa780eb11398c8c48
elliotyagami <eatdmaxe@gmail.com>
2018-09-12 02:33:56 +0530

    gates functionality

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 rm -rf ../test/*
-cp index.html ../../kontra.min.js  ../test/
+cp index.html kontra.min.js  ../test/
 mkdir -p ../test/assets
 cp assets/terrain.json assets/new.png ../test/assets
 cd ..
