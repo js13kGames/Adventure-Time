fa61cc2cd94e23a982bcbfc6e608d824b63c5a76
elliotyagami <eatdmaxe@gmail.com>
2018-09-11 14:20:31 +0530

    added basic sprite map and terrain

---
+++ script.sh
@@ -0,0 +1,10 @@
+rm -rf ../test/*
+cp index.html ../../kontra.min.js  ../test/
+mkdir -p ../test/assets
+cp assets/terrain.json assets/new.png ../test/assets
+cd ..
+du test
+tree test
+zip -r test.zip test
+du test.zip
+cd tileEngine
