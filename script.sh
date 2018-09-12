3822cb95b9ad94edbeea0cc5742e2cf6aa9c5f54
elliotyagami <eatdmaxe@gmail.com>
2018-09-12 20:00:42 +0530

    unified code further

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 rm -rf ../test/*
+rm -rf ../test.zip
 cp index.html kontra.min.js  ../test/
 mkdir -p ../test/assets
-cp assets/terrain.json assets/new.png ../test/assets
+cp assets/new1.png ../test/assets/new.png
+cp assets/terrain.json ../test/assets/
 cd ..
 du test
 tree test
